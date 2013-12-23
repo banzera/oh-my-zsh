d622ccde04b96aeea5c41636b124f695016f1f37
Bryan Banz <bryan.banz@propylon.com>
2013-12-23 13:44:29 -0600

    Updated custom aliases, functions

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -1,3 +1,16 @@
 function push-key {
   ssh $1 "echo '`cat ~/.ssh/id_rsa.pub`' >> ~/.ssh/authorized_keys"
 }
+
+function push-key-d {
+  ssh $1 "echo '`cat ~/.ssh/id_dsa.pub`' >> ~/.ssh/authorized_keys"
+}
+
+# echo the paths in $PATH, in the order they're searched
+function pp {
+  PATHS_ARRAY=("${(s/:/)PATH}")
+
+  for P in $PATHS_ARRAY; do
+    echo $P
+  done
+}
